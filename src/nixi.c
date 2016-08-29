031bd6ea8a8093c074ea96f280b88db0bb8660a6
Dominic Schaff <dominic.schaff@gmail.com>
2016-08-29 17:47:17 +0200

    Removed all backgrounds, and cleaned up some code

--- src/nixi.c
+++ src/nixi.c
@@ -11,7 +11,6 @@
 
 // Piece Sizing
 #define BATTERY_HEIGHT 5
-#define TIME_BAR_WIDTH 20
 #define PADDING 5
 #define SUB_TEXT_HEIGHT 30
 #define TITLE_TEXT_HEIGHT 50
@@ -22,8 +21,8 @@ static Window *s_main_window;
 static TextLayer *s_time_text_layer, *s_date_text_layer;
 static TextLayer *s_steps_text_layer, *s_steps_perc_text_layer;
 static TextLayer *s_battery_text_layer;
-static int s_battery_level, s_hour_level, s_minute_level, s_steps_level, s_steps_average, s_steps_average_now;
-static Layer *s_battery_layer, *s_bluetooth_layer, *s_hour_layer, *s_minute_layer, *s_steps_layer, *s_steps_now_layer;
+static int s_hour_level, s_minute_level, s_steps_level, s_steps_average, s_steps_average_now;
+static Layer *s_bluetooth_layer, *s_time_layer, *s_steps_layer, *s_steps_now_layer;
 static bool bluetooth_connected;
 
 // All functions used
@@ -32,18 +31,14 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 static void battery_callback(BatteryChargeState state);
 static void main_window_load(Window *window);
 static void main_window_unload(Window *window);
-static void battery_update_proc(Layer *layer, GContext *ctx);
 static void init();
 static void deinit();
 static void bluetooth_callback(bool connected);
 static void bluetooth_update_proc(Layer *layer, GContext *ctx);
-static void hour_update_proc(Layer *layer, GContext *ctx);
-static void minute_proc_layer(Layer *layer, GContext *ctx);
+static void time_update_proc(Layer *layer, GContext *ctx);
 static void steps_proc_layer(Layer *layer, GContext *ctx);
 static void steps_now_proc_layer(Layer *layer, GContext *ctx);
-static void add_text_layer(Layer *window_layer, TextLayer *text_layer);
-static void add_text_layer_right(Layer *window_layer, TextLayer *text_layer);
-static void add_text_layer_left(Layer *window_layer, TextLayer *text_layer);
+static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment);
 
 /**
  * Main function, the watch runs this function
@@ -98,7 +93,7 @@ static void main_window_load(Window *window)
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
-  
+
   int hh = bounds.size.h>>1;
   int hw = bounds.size.w>>1;
 
@@ -118,70 +113,41 @@ static void main_window_load(Window *window)
   layer_add_child(window_get_root_layer(window), s_steps_now_layer);
 
   // Create hour meter Layer
-  s_hour_layer = layer_create(GRect(0, hh, hw, hh));
-  layer_set_update_proc(s_hour_layer, hour_update_proc);
-  layer_add_child(window_get_root_layer(window), s_hour_layer);
-
-  // Create minute meter Layer
-  s_minute_layer = layer_create(GRect(hw, hh, hw, hh));
-  layer_set_update_proc(s_minute_layer, minute_proc_layer);
-  layer_add_child(window_get_root_layer(window), s_minute_layer);
+  s_time_layer = layer_create(GRect(0, hh, bounds.size.w, hh));
+  layer_set_update_proc(s_time_layer, time_update_proc);
+  layer_add_child(window_get_root_layer(window), s_time_layer);
 
   // Create the time display
   // I can't seem to calculate 52 from the other values, I will find a way (or at least in a way that makes sense)
   s_time_text_layer = text_layer_create(GRect(0, 52, bounds.size.w, TITLE_TEXT_HEIGHT));
   text_layer_set_font(s_time_text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_BOLD_SUBSET_49));
-  add_text_layer(window_layer, s_time_text_layer);
+  add_text_layer(window_layer, s_time_text_layer, GTextAlignmentCenter);
 
   // Create the date display
   s_date_text_layer = text_layer_create(GRect(0, 52 + TITLE_TEXT_HEIGHT, bounds.size.w, SUB_TEXT_HEIGHT));
   text_layer_set_font(s_date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  add_text_layer_right(window_layer, s_date_text_layer);
+  add_text_layer(window_layer, s_date_text_layer, GTextAlignmentRight);
 
   // Create the steps display
   s_steps_text_layer = text_layer_create(GRect(0, HEIGHT - SUB_TEXT_HEIGHT, bounds.size.w, SUB_TEXT_HEIGHT));
   text_layer_set_font(s_steps_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  add_text_layer_left(window_layer, s_steps_text_layer);
+  add_text_layer(window_layer, s_steps_text_layer, GTextAlignmentLeft);
 
   // Create the steps percentage display
   s_steps_perc_text_layer = text_layer_create(GRect(0, bounds.size.h - SUB_TEXT_HEIGHT, bounds.size.w, SUB_TEXT_HEIGHT));
   text_layer_set_font(s_steps_perc_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
-  add_text_layer_right(window_layer, s_steps_perc_text_layer);
-  
+  add_text_layer(window_layer, s_steps_perc_text_layer, GTextAlignmentRight);
+
   s_battery_text_layer = text_layer_create(GRect(0, 0, WIDTH, SUB_TEXT_HEIGHT));
   text_layer_set_font(s_battery_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18));
-  add_text_layer_left(window_layer, s_battery_text_layer);
-
-  // Create battery meter Layer
-  s_battery_layer = layer_create(GRect(0, 0, WIDTH, BATTERY_HEIGHT));
-  layer_set_update_proc(s_battery_layer, battery_update_proc);
-  layer_add_child(window_get_root_layer(window), s_battery_layer);
+  add_text_layer(window_layer, s_battery_text_layer, GTextAlignmentLeft);
 }
 
-static void add_text_layer(Layer *window_layer, TextLayer *text_layer)
+static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment)
 {
   text_layer_set_background_color(text_layer, GColorClear);
   text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text(text_layer, "--");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-}
-
-static void add_text_layer_right(Layer *window_layer, TextLayer *text_layer)
-{
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text(text_layer, "--");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentRight);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
-}
-
-static void add_text_layer_left(Layer *window_layer, TextLayer *text_layer)
-{
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text(text_layer, "--");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(text_layer, alignment);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
@@ -197,11 +163,10 @@ static void main_window_unload(Window *window)
   text_layer_destroy(s_steps_text_layer);
   text_layer_destroy(s_steps_perc_text_layer);
   text_layer_destroy(s_battery_text_layer);
-  layer_destroy(s_battery_layer);
   layer_destroy(s_bluetooth_layer);
-  layer_destroy(s_hour_layer);
-  layer_destroy(s_minute_layer);
+  layer_destroy(s_time_layer);
   layer_destroy(s_steps_layer);
+  layer_destroy(s_steps_now_layer);
 }
 
 /**
@@ -222,8 +187,9 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
  */
 static void battery_callback(BatteryChargeState state)
 {
-  s_battery_level = state.charge_percent;
-  layer_mark_dirty(s_battery_layer);
+  static char battery_buffer[10];
+  snprintf(battery_buffer, sizeof(battery_buffer), "%d%%", state.charge_percent);
+  text_layer_set_text(s_battery_text_layer, battery_buffer);
 }
 
 /**
@@ -234,35 +200,12 @@ static void battery_callback(BatteryChargeState state)
 static void bluetooth_callback(bool connected) {
   // Show icon if disconnected
   bluetooth_connected = connected;
-  layer_mark_dirty(s_bluetooth_layer);
+  layer_set_hidden(s_bluetooth_layer, connected);
 
   if(!connected) {
     // Issue a vibrating alert
     vibes_double_pulse();
   }
-}
-
-/**
- * When the battery layer is marked as dirty run this
- *
- * @param layer The layer to update
- * @param ctx   The context
- */
-static void battery_update_proc(Layer *layer, GContext *ctx)
-{
-  GRect bounds = layer_get_bounds(layer);
-
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-
-  // Draw the bar
-  graphics_context_set_fill_color(ctx, GColorBlue);
-  graphics_fill_rect(ctx, GRect(0, 0, (int)(float)(((float)s_battery_level / 100.0F) * WIDTHf), bounds.size.h), 0, GCornerNone);
-  
-  static char battery_buffer[10];
-  snprintf(battery_buffer, sizeof(battery_buffer), "%d%%", s_battery_level);
-  text_layer_set_text(s_battery_text_layer, battery_buffer);
 }
 
 /**
@@ -273,15 +216,8 @@ static void battery_update_proc(Layer *layer, GContext *ctx)
  */
 static void bluetooth_update_proc(Layer *layer, GContext *ctx)
 {
-  GRect bounds = layer_get_bounds(layer);
-
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-
-  // Draw the bar
   graphics_context_set_fill_color(ctx, GColorRed);
-  graphics_fill_rect(ctx, GRect(0, 0, bluetooth_connected ? 0 : bounds.size.w, bounds.size.h), 0, GCornerNone);
+  graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
 /**
@@ -290,37 +226,19 @@ static void bluetooth_update_proc(Layer *layer, GContext *ctx)
  * @param layer The layer to update
  * @param ctx   The context
  */
-static void hour_update_proc(Layer *layer, GContext *ctx)
+static void time_update_proc(Layer *layer, GContext *ctx)
 {
   GRect bounds = layer_get_bounds(layer);
 
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
   float l = s_hour_level / 24.0f;
-  
+
   graphics_context_set_fill_color(ctx, GColorPictonBlue);
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, DEG_TO_TRIGANGLE(l*360));
-}
 
-/**
- * When the minute layer is marked as dirty run thi
- *
- * @param layer The layer to update
- * @param ctx   The context
- */
-static void minute_proc_layer(Layer *layer, GContext *ctx)
-{
-  GRect bounds = layer_get_bounds(layer);
+  l = s_minute_level / 60.0f;
 
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-  
-  float l = s_minute_level / 60.0f;
-  
   graphics_context_set_fill_color(ctx, GColorMagenta);
-  graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, DEG_TO_TRIGANGLE(l*360));
+  graphics_fill_radial(ctx, GRect(bounds.size.w>>3, bounds.size.h>>3, (3*bounds.size.w)>>2, (3*bounds.size.h)>>2), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, DEG_TO_TRIGANGLE(l*360));
 }
 
 /**
@@ -333,15 +251,11 @@ static void steps_proc_layer(Layer *layer, GContext *ctx)
 {
   GRect bounds = layer_get_bounds(layer);
 
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-
   float l = 1.0f*s_steps_level/s_steps_average;
 
   graphics_context_set_fill_color(ctx, l >= 1.0 ? GColorMalachite : GColorShockingPink);
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, DEG_TO_TRIGANGLE(l*360));
-  
+
   l = 1.0f*s_steps_average_now/s_steps_average;
   if (l <= 1.0) {
     graphics_context_set_fill_color(ctx, GColorBlack);
@@ -359,12 +273,8 @@ static void steps_now_proc_layer(Layer *layer, GContext *ctx)
 {
   GRect bounds = layer_get_bounds(layer);
 
-  // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
-
   float l = 1.0f*s_steps_level/s_steps_average_now;
-  
+
   if (l>1.3) {
     l = 1.3;
   }
@@ -372,14 +282,14 @@ static void steps_now_proc_layer(Layer *layer, GContext *ctx)
     l = 0.3;
   }
   l -= 1.0;
-  
+
   // draw the meters:
   graphics_context_set_fill_color(ctx, GColorFolly);
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, DEG_TO_TRIGANGLE(0), DEG_TO_TRIGANGLE(5));
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, DEG_TO_TRIGANGLE(355), DEG_TO_TRIGANGLE(360));
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, DEG_TO_TRIGANGLE(40), DEG_TO_TRIGANGLE(50));
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, DEG_TO_TRIGANGLE(310), DEG_TO_TRIGANGLE(320));
-  
+
   graphics_context_set_fill_color(ctx, GColorBlack);
   graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS<<1, DEG_TO_TRIGANGLE(180*l), DEG_TO_TRIGANGLE(180*l+5));
 }
@@ -431,15 +341,14 @@ static void update_time()
     snprintf(steps_buffer, sizeof(steps_buffer), "%d", s_steps_level);
   }
   snprintf(steps_perc_buffer, sizeof(steps_buffer), "%d%%", step_perc);
-  
+
 
   // Set the steps display
   text_layer_set_text(s_steps_text_layer, steps_buffer);
   text_layer_set_text(s_steps_perc_text_layer, steps_perc_buffer);
 
   // Mark the layers as dirty
-  layer_mark_dirty(s_hour_layer);
-  layer_mark_dirty(s_minute_layer);
+  layer_mark_dirty(s_time_layer);
   layer_mark_dirty(s_steps_layer);
   layer_mark_dirty(s_steps_now_layer);
 }
