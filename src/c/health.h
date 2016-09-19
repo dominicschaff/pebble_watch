8a1a30a796fc160a104378f9f2a2076db8bd2815
Dominic Schaff <dominic.schaff@gmail.com>
2016-09-19 19:31:55 +0200

    Moved all source into single file again (hopefully benefitting battery life

--- src/c/health.h
+++
@@ -1,9 +0,0 @@
-#include <pebble.h>
-
-void update_health();
-
-void health_load_graphics(Window *window);
-void health_load_text(Window *window);
-void health_unload(Window *window);
-void health_init();
-void health_deinit();
--- src/c/nixi.c
+++ src/c/nixi.c
@@ -1,25 +1,45 @@
 #include <pebble.h>
-#include "constants.h"
-#include "timedisplay.h"
-#include "health.h"
+// Default value
+#define STEPS_DEFAULT 1000
 
-// All of the global variables
+// Piece Sizing
+#define SUB_TEXT_HEIGHT 30
+#define TITLE_TEXT_HEIGHT 49
+#define PIE_THICKNESS 20
+
 static Window *s_main_window;
 
+static GFont s_time_font;
+
+static Layer *s_bluetooth_layer;
+static Layer *s_steps_layer, *s_steps_now_layer;
+static Layer *s_time_layer;
+
 static TextLayer *s_battery_text_layer;
+static TextLayer *s_steps_text_layer, *s_steps_perc_text_layer, *s_steps_now_average_text_layer, *s_steps_average_text_layer;
+static TextLayer *s_time_hour_text_layer, *s_time_minute_text_layer, *s_date_text_layer, *s_day_text_layer;
 
 static int s_battery_level;
-static Layer *s_bluetooth_layer;
+static int s_hour_level, s_minute_level;
+static int s_steps_level, s_steps_average, s_steps_average_now;
 
-// All functions used
-static void battery_callback(BatteryChargeState state);
+static void deinit();
+static void init();
 static void main_window_load(Window *window);
 static void main_window_unload(Window *window);
-static void init();
-static void deinit();
+
+static void battery_callback(BatteryChargeState state);
 static void bluetooth_callback(bool connected);
+
 static void bluetooth_update_proc(Layer *layer, GContext *ctx);
+static void steps_now_proc_layer(Layer *layer, GContext *ctx);
+static void steps_proc_layer(Layer *layer, GContext *ctx);
+static void time_update_proc(Layer *layer, GContext *ctx);
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+
 static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment);
+static void update_watch();
 
 /**
  * Main function, the watch runs this function
@@ -48,8 +68,9 @@ static void init()
   connection_service_subscribe((ConnectionHandlers) {
     .pebble_app_connection_handler = bluetooth_callback
   });
-  timedisplay_init();
-  health_init();
+  update_watch();
+  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  s_steps_average = STEPS_DEFAULT;
 
   battery_callback(battery_state_service_peek());
   battery_state_service_subscribe(battery_callback);
@@ -60,7 +81,6 @@ static void init()
  */
 static void deinit()
 {
-  timedisplay_deinit();
   window_destroy(s_main_window);
 }
 
@@ -83,23 +103,66 @@ static void main_window_load(Window *window)
   layer_set_update_proc(s_bluetooth_layer, bluetooth_update_proc);
   layer_add_child(window_layer, s_bluetooth_layer);
 
-  timedisplay_load_graphics(window);
-  health_load_graphics(window);
-  timedisplay_load_text(window);
-  health_load_text(window);
+  // Create hour meter Layer
+  s_time_layer = layer_create(bounds);
+  layer_set_update_proc(s_time_layer, time_update_proc);
+  layer_add_child(window_layer, s_time_layer);
+
+  // Create steps meter Layer
+  s_steps_layer = layer_create(bounds);
+  layer_set_update_proc(s_steps_layer, steps_proc_layer);
+  layer_add_child(window_layer, s_steps_layer);
+
+  // Create steps meter Layer
+  s_steps_now_layer = layer_create(GRect(hw>>1, hh>>1, hw, hh));
+  layer_set_update_proc(s_steps_now_layer, steps_now_proc_layer);
+  layer_add_child(window_layer, s_steps_now_layer);
+
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_PIXELS_49));
+
+  // Create the time display
+  s_time_hour_text_layer = text_layer_create(GRect(0, SUB_TEXT_HEIGHT, bounds.size.w, TITLE_TEXT_HEIGHT));
+  text_layer_set_font(s_time_hour_text_layer, s_time_font);
+  add_text_layer(window_layer, s_time_hour_text_layer, GTextAlignmentCenter);
+
+  s_time_minute_text_layer = text_layer_create(GRect(0, SUB_TEXT_HEIGHT +  TITLE_TEXT_HEIGHT, bounds.size.w, TITLE_TEXT_HEIGHT));
+  text_layer_set_font(s_time_minute_text_layer, s_time_font);
+  add_text_layer(window_layer, s_time_minute_text_layer, GTextAlignmentCenter);
+
+  // Create the date display
+  s_date_text_layer = text_layer_create(GRect(0, -5, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_date_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  add_text_layer(window_layer, s_date_text_layer, GTextAlignmentLeft);
+
+  // Create the date display
+  s_day_text_layer = text_layer_create(GRect(0, SUB_TEXT_HEIGHT - 13, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_day_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  add_text_layer(window_layer, s_day_text_layer, GTextAlignmentLeft);
+
+  // Create the steps display
+  s_steps_text_layer = text_layer_create(GRect(0, bounds.size.h - SUB_TEXT_HEIGHT, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_steps_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  add_text_layer(window_layer, s_steps_text_layer, GTextAlignmentLeft);
+
+  // Create the steps percentage display
+  s_steps_perc_text_layer = text_layer_create(GRect(0, bounds.size.h - SUB_TEXT_HEIGHT, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_steps_perc_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  add_text_layer(window_layer, s_steps_perc_text_layer, GTextAlignmentRight);
+
+  // Create the current average steps display
+  s_steps_now_average_text_layer = text_layer_create(GRect(0, bounds.size.h - SUB_TEXT_HEIGHT - 13, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_steps_now_average_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  add_text_layer(window_layer, s_steps_now_average_text_layer, GTextAlignmentLeft);
+
+  // Create the average steps display
+  s_steps_average_text_layer = text_layer_create(GRect(0, bounds.size.h - SUB_TEXT_HEIGHT - 13, bounds.size.w, SUB_TEXT_HEIGHT));
+  text_layer_set_font(s_steps_average_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  add_text_layer(window_layer, s_steps_average_text_layer, GTextAlignmentRight);
 
   // Create the battery percentage display
   s_battery_text_layer = text_layer_create(GRect(0, -5, bounds.size.w, SUB_TEXT_HEIGHT));
   text_layer_set_font(s_battery_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
   add_text_layer(window_layer, s_battery_text_layer, GTextAlignmentRight);
-}
-
-static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment)
-{
-  text_layer_set_background_color(text_layer, GColorClear);
-  text_layer_set_text_color(text_layer, GColorBlack);
-  text_layer_set_text_alignment(text_layer, alignment);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
 }
 
 /**
@@ -109,8 +172,20 @@ static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlig
  */
 static void main_window_unload(Window *window)
 {
-  timedisplay_unload(window);
-  health_unload(window);
+  text_layer_destroy(s_time_hour_text_layer);
+  text_layer_destroy(s_time_minute_text_layer);
+  text_layer_destroy(s_date_text_layer);
+  text_layer_destroy(s_day_text_layer);
+  layer_destroy(s_time_layer);
+  fonts_unload_custom_font(s_time_font);
+
+  text_layer_destroy(s_steps_text_layer);
+  text_layer_destroy(s_steps_perc_text_layer);
+  text_layer_destroy(s_steps_now_average_text_layer);
+  text_layer_destroy(s_steps_average_text_layer);
+  layer_destroy(s_steps_layer);
+  layer_destroy(s_steps_now_layer);
+
   text_layer_destroy(s_battery_text_layer);
   layer_destroy(s_bluetooth_layer);
 }
@@ -126,7 +201,7 @@ static void battery_callback(BatteryChargeState state)
   s_battery_level = state.charge_percent;
   snprintf(battery_buffer, sizeof(battery_buffer), "%d%%%s", state.charge_percent, state.is_charging ? " +" : "");
   text_layer_set_text(s_battery_text_layer, battery_buffer);
-  update_time();
+  update_watch();
 }
 
 /**
@@ -142,6 +217,17 @@ static void bluetooth_callback(bool connected)
 }
 
 /**
+ * On every tick run this function
+ *
+ * @param tick_time     The orriginal time
+ * @param units_changed What units changed
+ */
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed)
+{
+  update_watch();
+}
+
+/**
  * When the bluetooth layer is marked as dirty run thi
  *
  * @param layer The layer to update
@@ -153,9 +239,154 @@ static void bluetooth_update_proc(Layer *layer, GContext *ctx)
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
-/*
+/**
+ * When the hour layer is marked as dirty run thi
+ *
+ * @param layer The layer to update
+ * @param ctx   The context
+ */
+static void time_update_proc(Layer *layer, GContext *ctx)
+{
+  GRect bounds = layer_get_bounds(layer);
 
+  graphics_context_set_fill_color(ctx, GColorPictonBlue);
+  graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, s_hour_level * DEG_TO_TRIGANGLE(15));
+
+  graphics_context_set_fill_color(ctx, GColorMagenta);
+  graphics_fill_radial(ctx, GRect(bounds.size.w >> 3, bounds.size.h >> 3, (3 * bounds.size.w) >> 2, (3 * bounds.size.h) >> 2), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, s_minute_level * DEG_TO_TRIGANGLE(6));
+}
+
+/**
+ * When the steps layer is marked as dirty run this
+ *
+ * @param layer The layer to update
+ * @param ctx   The context
+ */
+static void steps_proc_layer(Layer *layer, GContext *ctx)
+{
+  GRect bounds = layer_get_bounds(layer);
+
+  float l = 1.0f * s_steps_level / s_steps_average;
+
+  graphics_context_set_fill_color(ctx, l >= 1.0 ? GColorMalachite : GColorShockingPink);
+  graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, PIE_THICKNESS >> 2, 0, l * DEG_TO_TRIGANGLE(360));
+
+  l = 1.0f * s_steps_average_now / s_steps_average;
+  if (l <= 1.0) {
+    graphics_context_set_fill_color(ctx, GColorImperialPurple);
+    graphics_fill_radial(ctx, bounds, GOvalScaleModeFitCircle, PIE_THICKNESS >> 1, l * DEG_TO_TRIGANGLE(360) - DEG_TO_TRIGANGLE(2), l * DEG_TO_TRIGANGLE(360) + DEG_TO_TRIGANGLE(2));
+  }
+}
+
+/**
+ * When the steps layer is marked as dirty run thi
+ *
+ * @param layer The layer to update
+ * @param ctx   The context
+ */
+static void steps_now_proc_layer(Layer *layer, GContext *ctx)
+{
+  GRect bounds = layer_get_bounds(layer);
+
+  float l = 1.0f * s_steps_level / s_steps_average_now;
+
+  l = DEG_TO_TRIGANGLE((((l > 2) ? 1.0 : ((l < 0) ? -1.0 : l - 1.0))) * 180);
 
   graphics_context_set_fill_color(ctx, GColorLavenderIndigo);
-  graphics_fill_radial(ctx, GRect((bounds.size.w >> 3) + PIE_THICKNESS, (bounds.size.h >> 3) + PIE_THICKNESS, ((3 * bounds.size.w) >> 2) - (PIE_THICKNESS << 1), ((3 * bounds.size.h) >> 2) - (PIE_THICKNESS << 1)), GOvalScaleModeFitCircle, PIE_THICKNESS >> 1, 0, s_battery_level * DEG_TO_TRIGANGLE(3.6));
+  if (l < 0) {
+    graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS << 1, DEG_TO_TRIGANGLE(360) + l, DEG_TO_TRIGANGLE(360));
+  } else {
+    graphics_fill_radial(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), GOvalScaleModeFitCircle, PIE_THICKNESS << 1, 0, l);
+  }
+
+}
+
+/* Watch update for tick: */
+
+/**
+ * When the time is updated run this to update the various components
  */
+void update_watch()
+{
+  static char s_hour_buffer[4];
+  static char s_minute_buffer[4];
+  static char date_buffer[16];
+  static char day_buffer[16];
+  static char steps_buffer[10];
+  static char steps_perc_buffer[10];
+  static char steps_now_buffer[15];
+  static char steps_average_buffer[10];
+
+  // Get the current time
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Save the minute and hour (for the bars)
+  s_hour_level = tick_time->tm_hour;
+  s_minute_level = tick_time->tm_min;
+
+  // Create the string for the time display
+  strftime(s_hour_buffer, sizeof(s_hour_buffer), clock_is_24h_style() ?"%H" : "%I", tick_time);
+  text_layer_set_text(s_time_hour_text_layer, s_hour_buffer);
+  strftime(s_minute_buffer, sizeof(s_minute_buffer), "%M", tick_time);
+  text_layer_set_text(s_time_minute_text_layer, s_minute_buffer);
+
+  // Create the string for the date display
+  strftime(date_buffer, sizeof(date_buffer), "%d %b", tick_time);
+  strftime(day_buffer, sizeof(day_buffer), "%a", tick_time);
+  text_layer_set_text(s_date_text_layer, date_buffer);
+  text_layer_set_text(s_day_text_layer, day_buffer);
+
+  // Mark the layers as dirty
+  layer_mark_dirty(s_time_layer);
+
+  int start = time_start_of_today();
+
+  s_steps_average = (int)health_service_sum_averaged(HealthMetricStepCount, start, start + SECONDS_PER_DAY, HealthServiceTimeScopeDailyWeekdayOrWeekend);
+  if (s_steps_average < 1)
+    s_steps_average = STEPS_DEFAULT;
+
+  s_steps_average_now = (int)health_service_sum_averaged(HealthMetricStepCount, start, time(NULL), HealthServiceTimeScopeDailyWeekdayOrWeekend);
+
+  if (s_steps_average_now < 1)
+    s_steps_average_now = STEPS_DEFAULT;
+
+  s_steps_level = (int)health_service_sum_today(HealthMetricStepCount);
+
+  if (s_steps_level > 1000)
+    snprintf(steps_buffer, sizeof(steps_buffer), "%d,%03d", s_steps_level / 1000, s_steps_level % 1000);
+  else
+    snprintf(steps_buffer, sizeof(steps_buffer), "%d", s_steps_level);
+
+  if (s_steps_average > 1000)
+    snprintf(steps_average_buffer, sizeof(steps_average_buffer), "/%d,%03d", s_steps_average / 1000, s_steps_average % 1000);
+  else
+    snprintf(steps_average_buffer, sizeof(steps_average_buffer), "/%d", s_steps_average);
+
+  if (s_steps_average_now > 1000)
+    snprintf(steps_now_buffer, sizeof(steps_now_buffer), "%d,%03d", s_steps_average_now / 1000, s_steps_average_now % 1000);
+  else
+    snprintf(steps_now_buffer, sizeof(steps_now_buffer), "%d", s_steps_average_now);
+
+  snprintf(steps_perc_buffer, sizeof(steps_perc_buffer), "%d%%", (int)(100.0f * s_steps_level / s_steps_average));
+
+  // Set the steps display
+  text_layer_set_text(s_steps_text_layer, steps_buffer);
+  text_layer_set_text(s_steps_perc_text_layer, steps_perc_buffer);
+  text_layer_set_text(s_steps_now_average_text_layer, steps_now_buffer);
+  text_layer_set_text(s_steps_average_text_layer, steps_average_buffer);
+
+  // Mark the layers as dirty
+  layer_mark_dirty(s_steps_layer);
+  layer_mark_dirty(s_steps_now_layer);
+}
+
+/* Helper functions */
+
+static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment)
+{
+  text_layer_set_background_color(text_layer, GColorClear);
+  text_layer_set_text_color(text_layer, GColorBlack);
+  text_layer_set_text_alignment(text_layer, alignment);
+  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
