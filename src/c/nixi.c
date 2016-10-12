78d5e05bc6ccbcd9c8322a6139d4eb42f295c2db
Dominic Schaff <0merlin@users.noreply.github.com>
2016-10-12 13:16:03 +0200

    Made some small improvements for battery

--- src/c/nixi.c
+++ src/c/nixi.c
@@ -13,7 +13,7 @@ static GFont s_time_font;
 
 static Layer *s_bluetooth_layer;
 static Layer *s_steps_layer, *s_steps_now_layer;
-static Layer *s_time_layer;
+static Layer *s_hour_layer, *s_minute_layer;
 
 static TextLayer *s_battery_text_layer;
 static TextLayer *s_steps_text_layer, *s_steps_perc_text_layer, *s_steps_now_average_text_layer, *s_steps_average_text_layer;
@@ -34,13 +34,15 @@ static void bluetooth_callback(bool connected);
 static void bluetooth_update_proc(Layer *layer, GContext *ctx);
 static void steps_now_proc_layer(Layer *layer, GContext *ctx);
 static void steps_proc_layer(Layer *layer, GContext *ctx);
-static void time_update_proc(Layer *layer, GContext *ctx);
+static void time_hour_update_proc(Layer *layer, GContext *ctx);
+static void time_minute_update_proc(Layer *layer, GContext *ctx);
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
 
 static void add_text_layer(Layer *window_layer, TextLayer *text_layer, GTextAlignment alignment);
 static void format_number(char *str, int size, int number);
 static void update_watch();
+static void update_health();
 
 /**
  * Main function, the watch runs this function
@@ -72,6 +74,7 @@ static void init()
     .pebble_app_connection_handler = bluetooth_callback
   });
   update_watch();
+  update_health();
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
   s_steps_average = STEPS_DEFAULT;
 
@@ -107,9 +110,14 @@ static void main_window_load(Window *window)
   layer_add_child(window_layer, s_bluetooth_layer);
 
   // Create hour meter Layer
-  s_time_layer = layer_create(bounds);
-  layer_set_update_proc(s_time_layer, time_update_proc);
-  layer_add_child(window_layer, s_time_layer);
+  s_hour_layer = layer_create(GRect(PIE_THICKNESS, PIE_THICKNESS, bounds.size.w - (PIE_THICKNESS<<1), bounds.size.h - (PIE_THICKNESS<<1)));
+  layer_set_update_proc(s_hour_layer, time_hour_update_proc);
+  layer_add_child(window_layer, s_hour_layer);
+
+  // Create hour meter Layer
+  s_minute_layer = layer_create(GRect(bounds.size.w >> 3, bounds.size.h >> 3, (3 * bounds.size.w) >> 2, (3 * bounds.size.h) >> 2));
+  layer_set_update_proc(s_minute_layer, time_minute_update_proc);
+  layer_add_child(window_layer, s_minute_layer);
 
   // Create steps meter Layer
   s_steps_layer = layer_create(bounds);
@@ -179,7 +187,8 @@ static void main_window_unload(Window *window)
   text_layer_destroy(s_time_minute_text_layer);
   text_layer_destroy(s_date_text_layer);
   text_layer_destroy(s_day_text_layer);
-  layer_destroy(s_time_layer);
+  layer_destroy(s_hour_layer);
+  layer_destroy(s_minute_layer);
   fonts_unload_custom_font(s_time_font);
 
   text_layer_destroy(s_steps_text_layer);
@@ -248,15 +257,22 @@ static void bluetooth_update_proc(Layer *layer, GContext *ctx)
  * @param layer The layer to update
  * @param ctx   The context
  */
-static void time_update_proc(Layer *layer, GContext *ctx)
+static void time_hour_update_proc(Layer *layer, GContext *ctx)
 {
-  GRect bounds = layer_get_bounds(layer);
-
   graphics_context_set_fill_color(ctx, GColorPictonBlue);
-  graphics_fill_radial(ctx, GRect(PIE_THICKNESS, PIE_THICKNESS, bounds.size.w - (PIE_THICKNESS<<1), bounds.size.h - (PIE_THICKNESS<<1)), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, (s_hour_level%12) * DEG_TO_TRIGANGLE(30));
+  graphics_fill_radial(ctx, layer_get_bounds(layer), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, (s_hour_level%12) * DEG_TO_TRIGANGLE(30));
+}
 
+/**
+ * When the minute layer is marked as dirty run thi
+ *
+ * @param layer The layer to update
+ * @param ctx   The context
+ */
+static void time_minute_update_proc(Layer *layer, GContext *ctx)
+{
   graphics_context_set_fill_color(ctx, GColorMagenta);
-  graphics_fill_radial(ctx, GRect(bounds.size.w >> 3, bounds.size.h >> 3, (3 * bounds.size.w) >> 2, (3 * bounds.size.h) >> 2), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, s_minute_level * DEG_TO_TRIGANGLE(6));
+  graphics_fill_radial(ctx, layer_get_bounds(layer), GOvalScaleModeFitCircle, PIE_THICKNESS, 0, s_minute_level * DEG_TO_TRIGANGLE(6));
 }
 
 /**
@@ -309,16 +325,14 @@ static void steps_now_proc_layer(Layer *layer, GContext *ctx)
 /**
  * When the time is updated run this to update the various components
  */
-void update_watch()
+static void update_watch()
 {
   static char s_hour_buffer[4];
   static char s_minute_buffer[4];
   static char date_buffer[16];
   static char day_buffer[16];
-  static char steps_buffer[10];
-  static char steps_perc_buffer[10];
-  static char steps_now_buffer[15];
-  static char steps_average_buffer[10];
+  int tmp_hour = s_hour_level;
+  int tmp_minute = s_minute_level;
 
   // Get the current time
   time_t temp = time(NULL);
@@ -340,6 +354,25 @@ void update_watch()
   text_layer_set_text(s_date_text_layer, date_buffer);
   text_layer_set_text(s_day_text_layer, day_buffer);
 
+  // Mark the layers as dirty
+  if (tmp_hour != s_hour_level) {
+      layer_mark_dirty(s_hour_layer);
+  }
+  if (tmp_minute != s_minute_level) {
+      layer_mark_dirty(s_minute_layer);
+  }
+  if (s_minute_level % 5 == 0) {
+    update_health();
+  }
+}
+
+static void update_health()
+{
+  static char steps_buffer[10];
+  static char steps_perc_buffer[10];
+  static char steps_now_buffer[15];
+  static char steps_average_buffer[10];
+
   int start = time_start_of_today();
 
   s_steps_average = (int)health_service_sum_averaged(HealthMetricStepCount, start, start + SECONDS_PER_DAY, HealthServiceTimeScopeDailyWeekdayOrWeekend);
@@ -358,15 +391,14 @@ void update_watch()
   format_number(steps_now_buffer, sizeof(steps_now_buffer), s_steps_average_now);
 
   snprintf(steps_perc_buffer, sizeof(steps_perc_buffer), "%d%%", (int)(100.0f * s_steps_level / s_steps_average));
-
+  
   // Set the steps display
   text_layer_set_text(s_steps_text_layer, steps_buffer);
   text_layer_set_text(s_steps_perc_text_layer, steps_perc_buffer);
   text_layer_set_text(s_steps_now_average_text_layer, steps_now_buffer);
   text_layer_set_text(s_steps_average_text_layer, steps_average_buffer);
+  
 
-  // Mark the layers as dirty
-  layer_mark_dirty(s_time_layer);
   layer_mark_dirty(s_steps_layer);
   layer_mark_dirty(s_steps_now_layer);
 }
